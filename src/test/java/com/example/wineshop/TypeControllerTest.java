b914a9162c47b8a4595ccb195516bd25d6dfdbd3
Álvaro Real <105717687+loegGFT@users.noreply.github.com>
2022-05-27 09:37:56 +0200

    Expanded TypeControllerTest, still doesn't work

--- src/test/java/com/example/wineshop/TypeControllerTest.java
+++ src/test/java/com/example/wineshop/TypeControllerTest.java
@@ -2,25 +2,38 @@ package com.example.wineshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class TypeControllerTest {
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
-    WebTestClient webTestClient;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private int typeId = 23;
 
     @Test
-    void testType() {
-        webTestClient.get()
-                .uri("type")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$.name").isEqualTo("Chardonnay");
+    void testTypeNormal() throws Exception {
+        mockMvc.perform(get("/api/type/"+typeId)).andDo(print()).andExpect(status().isOk());
     }
 
+    @Test
+    void testTypeHAL() throws Exception {
+        mockMvc.perform(get("/api/type/hal/"+typeId)).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    void testTypeHAL2() throws Exception {
+        mockMvc.perform(get("/api/type/hal2/"+typeId)).andDo(print()).andExpect(status().is4xxClientError());
+    }
 }
